9e711bf1396a15499108e9e409be3412a9b8b0db
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-05-05 20:48:25 +0900

    specify inter class name

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,9 @@ const inter = Inter({
 
 export default function Layout({ children }: PropsWithChildren<{}>) {
   return (
-    <div className={`flex h-screen flex-col ${inter.variable}`}>
+    <div
+      className={`flex h-screen flex-col ${inter.variable} ${inter.className}`}
+    >
       <div className="container mx-auto px-4 py-8">
         <div className="rounded bg-white p-6 shadow">{children}</div>
       </div>
