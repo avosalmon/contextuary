42fdec275231472e6b1870f4b85bb53f37221c1d
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2023-05-05 20:47:12 +0900

    remove h1

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,8 +11,6 @@ export default function Layout({ children }: PropsWithChildren<{}>) {
   return (
     <div className={`flex h-screen flex-col ${inter.variable}`}>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="mb-4 text-2xl font-bold">Context-aware Translation</h1>
-
         <div className="rounded bg-white p-6 shadow">{children}</div>
       </div>
     </div>
